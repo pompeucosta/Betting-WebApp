657edaf6aff4823f09486905b07301b5e8d73e4a
Rafael Pinto <85125753+RafaelPBPinto@users.noreply.github.com>
2024-06-02 16:24:08 +0100

    remove unnecessary links

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -27,8 +27,7 @@ const Header = ({loggedIn}) => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#">Sports</Nav.Link>
-                    <Nav.Link href="#">Live</Nav.Link>
+                    
                 </Nav>
                 <Nav className='px-3'>
                     {loggedIn ? (
--- ui/src/components/Sidenav.jsx
+++ ui/src/components/Sidenav.jsx
@@ -9,14 +9,6 @@ const Sidenav = () => {
                     <Form.Control type="search" placeholder="Search" />
                 </Form.Group>
             </Form>
-            <div className="d-grid gap-2 text-center">
-                <h4>Top 5 Leagues</h4>
-                <Button variant="outline-primary" className="mb-3">Premier League</Button>
-                <Button variant="outline-primary" className="mb-3">La Liga</Button>
-                <Button variant="outline-primary" className="mb-3">Serie A</Button>
-                <Button variant="outline-primary" className="mb-3">Bundesliga</Button>
-                <Button variant="outline-primary" className="mb-3">Ligue 1</Button>
-            </div>
         </div>
     )
 }
