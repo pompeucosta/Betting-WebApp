269b5ab57266cd6b1e78672859f1140546540162
Ricardo-Antunes0 <ricardofantunes@ua.pt>
2024-06-02 23:57:37 +0100

    wallet button

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -2,11 +2,42 @@ import React from 'react'
 import { Navbar, Nav, Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import '../css/Header.css';
 
 
 const Header = () => {
     const [loggedIn, setLoggedIn] = useState(false);
+    const [walletBalance, setWalletBalance] = useState(0);
     const navigate = useNavigate();
+
+
+    const getWalletBalance = () => {
+        /*
+        // Get wallet balance
+        fetch('/wallet', {
+            method: 'GET',
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    response.json().then((data) => {
+                        setWalletBalance(data.balance);
+                    });
+                } else {
+                    console.error('Error:', response);
+                }
+            }).catch((error) => {
+                console.error('Error:', error);
+            });
+            */
+    };
+
+    const addMoneyWallet = () => {
+        // Add money to wallet
+        // Criar um form para adicionar dinheiro à carteira
+
+        //setWalletBalance(walletBalane + ...);
+        console.log('Dinheiro adicionado à carteira!');
+    };
     
     const handleProfileClick = () => {
         navigate('/profile');
@@ -28,6 +59,7 @@ const Header = () => {
           .then((response) => {
             if (response.status === 200) {
                 setLoggedIn(true);
+                getWalletBalance();
             } else {
                 setLoggedIn(false);
             }
@@ -46,7 +78,15 @@ const Header = () => {
                 </Nav>
                 <Nav className='px-3'>
                     {loggedIn ? (
+                        <>                        
+                        <Button className="walletButton" style={{ backgroundColor: 'rgb(186, 43, 4)', color: 'white', border:'none' }} onClick={addMoneyWallet}>
+                            <span className="walletIcon">
+                                <span className="walletIconPlus">+</span>
+                            </span>
+                            <span className="walletBalance">{walletBalance} €</span>
+                        </Button>
                         <Button variant="light" className="ms-2" onClick={handleProfileClick}>Profile</Button>
+                        </>
                     ) : (
                         <>
                             <Button variant="light" className="ms-2" onClick={handleRegisterClick}>Register</Button>
