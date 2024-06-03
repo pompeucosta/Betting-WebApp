24efdbd2a3dced13823668ba28c63b986817bf4a
Pompeu Costa <pompeu@ua.pt>
2024-06-03 20:46:43 +0100

    add env variables to web app

--- WebApp/Data/EnvVariables.cs
+++ WebApp/Data/EnvVariables.cs
@@ -2,8 +2,13 @@
 {
     public static class EnvVariables
     {
+        public static string LogsEnvVariableName { get; } = "LOGS_EXPORT_ENDPOINT";
         public static string LogsExportEndpoint { get; set; } = "http://localhost:3100";
+        public static string TracesEnvVariableName { get; } = "TRACES_EXPORT_ENDPOINT";
         public static string TracesExportEndpoint { get; set; } = "http://localhost:4317";
+        public static string BrokerEnvVariableName { get; } = "MQTT_ADDRESS";
         public static string BrokerAddress { get; set; } = "http://localhost:1883";
+        public static string DbEnvVariableName { get; } = "DATABASE_ADDRESS";
+        public static string DbAddress { get; set; } = "";
     }
 }
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -6,17 +6,34 @@ using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 
+var logsLocation = Environment.GetEnvironmentVariable(EnvVariables.LogsEnvVariableName);
+if(logsLocation != null)
+    EnvVariables.LogsExportEndpoint = logsLocation;
+
+var tracesLocation = Environment.GetEnvironmentVariable(EnvVariables.TracesEnvVariableName);
+if (tracesLocation != null)
+    EnvVariables.TracesExportEndpoint = tracesLocation;
+
+var brokerAddr = Environment.GetEnvironmentVariable(EnvVariables.BrokerEnvVariableName);
+if (brokerAddr != null)
+    EnvVariables.BrokerAddress = brokerAddr;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var dbAddr = Environment.GetEnvironmentVariable(EnvVariables.DbEnvVariableName);
+if (dbAddr != null)
+    EnvVariables.DbAddress = dbAddr;
+else
+    EnvVariables.DbAddress = builder.Configuration.GetConnectionString("DbConnection");
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"))
+    options.UseSqlServer(EnvVariables.DbAddress)
 );
 
 builder.Services.AddAuthorization();
