a05e40bb7c9a0f5b0afb22d729781c97aa355bbb
Rafael Pinto <85125753+RafaelPBPinto@users.noreply.github.com>
2024-06-03 17:05:42 +0100

    BetHistory done

--- ui/src/components/BetHistory.jsx
+++ ui/src/components/BetHistory.jsx
@@ -1,113 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
 
 const BetHistory = () => {
-
-    const test_bets = [
-        {
-            id: 1,
-            date: "2021-10-01",
-            amount: 100,
-            odd: 1.5,
-            outcome: "Team A",
-            game: {home: "Team A", away: "Team B"}
-        },
-        {
-            id: 2,
-            date: "2021-10-02",
-            amount: 200,
-            odd: 2.0,
-            outcome: "Team B",
-            game: {home: "Team C", away: "Team B"}
-        },
-        {
-            id: 3,
-            date: "2021-10-03",
-            amount: 300,
-            odd: 1.8,
-            outcome: "Team D",
-            game: {home: "Team A", away: "Team D"}
-        },
-        {
-            id: 4,
-            date: "2021-10-04",
-            amount: 400,
-            odd: 1.6,
-            outcome: "Team A",
-            game: {home: "Team A", away: "Team B"}
-        },
-        {
-            id: 5,
-            date: "2021-10-05",
-            amount: 500,
-            odd: 2.5,
-            outcome: "Team B",
-            game: {home: "Team C", away: "Team B"}
-        },
-        {
-            id: 6,
-            date: "2021-10-06",
-            amount: 600,
-            odd: 1.9,
-            outcome: "Team D",
-            game: {home: "Team A", away: "Team D"}
-        },
-        {
-            id: 7,
-            date: "2021-10-07",
-            amount: 700,
-            odd: 1.7,
-            outcome: "Team A",
-            game: {home: "Team A", away: "Team B"}
-        },
-        {
-            id: 8,
-            date: "2021-10-08",
-            amount: 800,
-            odd: 2.6,
-            outcome: "Team B",
-            game: {home: "Team C", away: "Team B"}
-        },
-        {
-            id: 9,
-            date: "2021-10-09",
-            amount: 900,
-            odd: 1.4,
-            outcome: "Team D",
-            game: {home: "Team A", away: "Team D"}
-        },
-        {
-            id: 10,
-            date: "2021-10-10",
-            amount: 1000,
-            odd: 1.3,
-            outcome: "Team A",
-            game: {home: "Team A", away: "Team B"}
-        },
-        {
-            id: 11,
-            date: "2021-10-11",
-            amount: 1100,
-            odd: 2.7,
-            outcome: "Team B",
-            game: {home: "Team C", away: "Team B"}
-        },
-        {
-            id: 12,
-            date: "2021-10-12",
-            amount: 1200,
-            odd: 1.2,
-            outcome: "Team D",
-            game: {home: "Team A", away: "Team D"}
-        }
-    ]
-    
     const itemsPerPage = 5;
     const [currentPage, setCurrentPage] = useState(1);
     const [searchTerm, setSearchTerm] = useState('');
+    const [bets, setBets] = useState([]);
 
-    const filteredBets = test_bets.filter(bet => 
-        `${bet.game.home} vs ${bet.game.away}`.toLowerCase().includes(searchTerm.toLowerCase())
+    useEffect(() => {
+        const fetchBets = async () => {
+            try {
+                const response = await fetch('/getBets');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch bets');
+                }
+                const data = await response.json();
+                console.log('bets:', data.bets);
+                setBets(data.bets);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+        fetchBets();
+    }, []);
+
+    const filteredBets = bets.filter(bet => 
+        `${bet.gameData.result.teams[0].name} vs ${bet.gameData.result.teams[1].name}`.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const indexOfLastItem = currentPage * itemsPerPage;
@@ -128,7 +46,7 @@ const BetHistory = () => {
                 <table className="table table-striped table-bordered">
                     <thead>
                         <tr>
-                            <th>Date</th>
+                            <th>Status</th>
                             <th>Game</th>
                             <th>Prediction</th>
                             <th>Odd</th>
@@ -136,13 +54,18 @@ const BetHistory = () => {
                         </tr>
                     </thead>
                     <tbody>
+                        
                         {currentItems.map(bet => (
-                            <tr key={bet.id}>
-                                <td>{bet.date}</td>
-                                <td>{bet.game.home} vs {bet.game.away}</td>
-                                <td>{bet.outcome}</td>
-                                <td>{bet.odd}</td>
-                                <td>{bet.amount}€</td>
+                            <tr key={bet.betID}>
+                                <td>{bet.gameData.result.status}</td>
+                                <td>{bet.gameData.result.teams[0].name} ({bet.gameData.result.homeGoals}) vs ({bet.gameData.result.awayGoals}) {bet.gameData.result.teams[1].name}</td>
+                                <td>{bet.betValue}</td>
+                                <td>
+                                    {bet.betValue === "Home" ? bet.oddsData.result.odds[0].values[0].odd :
+                                    bet.betValue === "Draw" ? bet.oddsData.result.odds[0].values[1].odd :
+                                    bet.betValue === "Away" ? bet.oddsData.result.odds[0].values[2].odd : null}
+                                </td>
+                                <td>{bet.amountPlaced}€</td>
                             </tr>
                         ))}
                     </tbody>
