a8b82757ed44a28d02bdb50979f98901979fcd86
Pompeu Costa <pompeu@ua.pt>
2024-06-03 15:38:18 +0100

    add mqtt function to send message to topic

--- WebApp/Data/EnvVariables.cs
+++ WebApp/Data/EnvVariables.cs
@@ -4,5 +4,6 @@
     {
         public static string LogsExportEndpoint { get; set; } = "http://localhost:3100";
         public static string TracesExportEndpoint { get; set; } = "http://localhost:4317";
+        public static string BrokerAddress { get; set; } = "http://localhost:1883";
     }
 }
---
+++ WebApp/Data/MQTT.cs
@@ -0,0 +1,35 @@
+﻿using MQTTnet.Client;
+using MQTTnet;
+
+namespace WebApp.Data
+{
+    public static class MQTT
+    {
+        public static async Task PublishMessageAsync(ILogger logger,string brokerAddress, string topic, string message)
+        {
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(brokerAddress)
+                .Build();
+
+            try
+            {
+                await mqttClient.ConnectAsync(options);
+            }catch(Exception ex)
+            {
+                logger.LogError($"failed to connect to broker at {brokerAddress} - {ex.Message}");
+                return;
+            }
+
+            var messageBuilder = new MqttApplicationMessageBuilder()
+            .WithTopic(topic)
+            .WithPayload(message)
+            .Build();
+
+            await mqttClient.PublishAsync(messageBuilder);
+            await mqttClient.DisconnectAsync();
+        }
+    }
+}
